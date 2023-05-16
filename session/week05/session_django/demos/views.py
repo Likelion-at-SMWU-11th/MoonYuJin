be91ed6e2d42884887b9fffbadde44340f28eb94
genieu99 <yujin0419@sookmyung.ac.kr>
2023-05-16 20:39:15 +0900

    [Feat] Moon's Django D는 묵음이야

---
+++ session/week05/session_django/demos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def helloBabyLion(request):
+    # return HttpResponse('장고의 세계로 들어왔습니다.')
+    # 이렇게 응답해줄거다
+    return render(request, 'crazyDjango.html')
