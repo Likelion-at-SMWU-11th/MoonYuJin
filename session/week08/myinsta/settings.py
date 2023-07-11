0b36b6eec1b63a919b07c8130e301fbf39f4d376
genieu99 <yujin0419@sookmyung.ac.kr>
2023-07-11 20:32:05 +0900

    [Feat] 9주차 세미나

--- session/week08/myinsta/settings.py
+++ session/week08/myinsta/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'myinsta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'myinsta', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- session/week08/posts/views.py
+++ session/week08/posts/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
+from django.views.generic import ListView
+from .models import Post
+
+def index(request):
+    return render(request, 'index.html')
+
+def post_list_view(request):
+    return render(request, 'posts/post_list.html')
+
+def post_detail_view(request, id):
+    return render(request, 'posts/post_detail.html')
+
+def post_create_view(request):
+    return render(request, 'posts/post_form.html')
+
+def post_delete_view(request, id):
+    return render(request, 'posts/post_confirm_delete.html')
+
+
+class class_view(ListView):
+    model = Post
+    template_name = 'cbv_view.html'
+    
 # Create your views here.
+def url_view(request):
+    data = {'code': '001', 'msg': 'OK'}
+    return HttpResponse('<h1>url_views</h1>')
+    # return JsonResponse(data)
+    # return HttpResponse('url.view')
+
+def url_parameter_view(request, username):
+    print('url_parameter_view()')
+    print(f'username: {username}')
+    print(f'request.GET: {request.GET}')
+    return HttpResponse(username)
+
+def function_view(request):
+    print(f'request.method: {request.method}')
+
+    if request.method == "GET":
+        print(f'request.GET: {request.GET}')
+    elif request.method == 'POST':
+        print(f'request.POST: {request.POST}')
+    return render(request, 'view.html')
